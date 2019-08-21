68d0aacfba88a1f737b704b35247ceca3dcd5234
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-08-20 23:42:08 -0700

    updates to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,36 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Oranges55!",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Oranges55!",
+//     database: "burgers_db"
+// });
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
 
 
-// // Connect to Heroku
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// // Connect to local host
-// else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'Oranges55!',
-//         database: 'burgers_db'
-//     });
-// };
+// Connect to Heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+// Connect to local host
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Oranges55!',
+        database: 'burgers_db'
+    });
+};
 
 module.exports = connection;
